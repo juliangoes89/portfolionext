edab1e5f8588a826d13f047a00deb21af0e153b8
juliangoes89 <juliangoes89@hotmail.com>
2025-01-31 19:51:32 -0500

    update client.ts

--- client.ts
+++ client.ts
@@ -1,5 +1,5 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
-
+//Apollo client
 export const client = new ApolloClient({
     uri: process.env.WP_GRAPHQL_URL,
 	cache: new InMemoryCache()
