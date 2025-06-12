a8c13e0dfaafedd666d8ef7f031c534006dc575d
Julian Gomez Espinal <juliangoes89@hotmail.com>
2025-06-11 23:22:26 -0500

    Portfolio basic projects and setup w/o functionality

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,7 +12,16 @@ export default {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        "green-color":"#27AE60",
+        "gray-color":"#828282",
+        "card-color":"#212121",
+        "color-background":"#171717",
+        "menu-color":"#172554"
       },
+      backgroundImage:{
+        "open-menu": `url('/menu.svg')`,
+        "close-menu": `url('/close.svg')`
+      }
     },
   },
   plugins: [],
